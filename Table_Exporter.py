30d8213019994a5cb4cee50cec1cbdc744325146
Kurt Drew <kurt.l.m.drew@gmail.com>
2018-05-13 14:34:30 +1200

    Add files via upload

--- Table_Exporter.py
+++ Table_Exporter.py
@@ -10,28 +10,39 @@ import pandas as pd
 import datetime
 
 def getSqlConnection(conn_string):
-    return da.connect(conn_string);
+    return da.connect(conn_string)
 
 def getSql(sql, conn_string,headers=None):
     cnxn = getSqlConnection(conn_string)
-    data = pd.read_sql_query(sql,cnxn)
-    if headers is not None:
-        data.columns = headers
+    try:
+        data = pd.read_sql_query(sql,cnxn)
+    except pd.io.sql.DatabaseError:
+        print("SQL DataBase Error")
+        data = None
+    else:
+        if headers is not None:
+            data.columns = headers
     return data
 
-def sqlRunner(table, sql_ser):
+def sqlRunner(table, sql_ser, out_type):
     sql = {'JDE':['JDE_PRODUCTION.PRODDTA.',CONNECTION_LVJDE],
            'GIS':['PCOGIS.SDE.',CONNECTION_GIS]}
     sql_cmd = """SELECT *
     FROM """ + sql[sql_ser][0] + table
-    try:
-        mydf = getSql(sql_cmd, sql[sql_ser][1])
-    except pyodbc.DatabaseError:
+    print("Querying table " + table)
+    mydf = getSql(sql_cmd, sql[sql_ser][1])
+    if mydf is None:
         print("Hey! " + table + " is not a table in " + sql_ser + ".")
     else:
-        excel_writer = pd.ExcelWriter(table + '.xlsx', engine='xlsxwriter')
-        mydf.to_excel(excel_writer, sheet_name='Data', index=False)
-        excel_writer.save()
+        print('Writing ' + table + ' to ' + out_type)
+        if out_type == 'EXCEL':
+            excel_writer = pd.ExcelWriter(table + '.xlsx', engine='xlsxwriter')
+            mydf.to_excel(excel_writer, sheet_name='Data', index=False)
+            excel_writer.save()
+        elif out_type == 'CSV':
+#            print('WIP')
+            mydf.to_csv(table + '.csv')
+
 
 CONNECTION_GIS = "Driver={SQL Server};SERVER=PWGISSQL01;DATABASE=PCOGIS;Trusted_Connection=yes"
 CONNECTION_LVJDE = "Driver={SQL Server};SERVER=PWJDESQL01;DATABASE=JDE_PRODUCTION;Trusted_Connection=yes"
@@ -47,27 +58,34 @@ while True:
     else:
         break
 
+outType = input("Is your perfered flavour EXCEL or CSV? ").upper()
+
+while True:
+    if outType not in ['EXCEL', 'CSV']:
+        outType = input("I'm sorry I don't know that flavour. Try again: ").upper()
+    else:
+        break
+
 listTables = []
 val = input("What table do you wish sir? (Press enter to end): ")
 
-
-
 while True:
     if val == "":
-        print("See ya sucker!")
+        print("Extracting now sir")
         break
     else:
-        listTables.append(val)
+        val = val.split('\n')
+        listTables.extend(val)
         val = input("Another sir? (Press enter to end): ")
 
 if len(listTables) == 0:
     print("""
           -------------------------
           Why did you run me then?!
-          -------------------------""")
+          -------------------------
+          """)
 else:
     for i in listTables:
-        sqlRunner(i, con_type)
+        sqlRunner(i, con_type, outType)
 
-
-print('Ending ' + str(datetime.datetime.now()))
+print("See ya sucker!" + str(datetime.datetime.now()))
